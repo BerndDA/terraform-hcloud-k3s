a70616283aa6e3b06d8b19e10501690c8530dced
Bernd Seidenspinner <bernd.seidenspinner@bayoo.net>
2022-06-23 18:40:55 +0200

    update variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,21 +1,25 @@
 variable "hetzner_token" {
   type    = string
   sensitive = true
+  description = "Hetzner cloud API token"
 }
 
 variable "cluster_name" {
   type    = string
   default = "dev"
+  description = "Name of your cluster. Also used to pre/postfix resources"
 }
 
 variable "image" {
   type    = string
   default = "debian-11"
+  description = "Type of OS image used for all nodes"
 }
 
 variable "k3s_version" {
   type    = string
   default = "v1.24.1+k3s1"
+  description = "K3S version to install on the nodes"
 }
 
 variable "main_pool_config" {
@@ -27,6 +31,7 @@ variable "main_pool_config" {
     num_nodes = 3
     node_type = "cx11"
   }
+  description = "Number and type of main nodes"
 }
 
 variable "worker_pool_config" {
@@ -38,4 +43,5 @@ variable "worker_pool_config" {
     num_nodes = 3
     node_type = "cx21"
   }
+  description = "Number and type of worker nodes"
 }
