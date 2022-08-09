9f8465ec2e18fa2f68122c55eed3fc89198e6533
Bernd Seidenspinner <bernd.seidenspinner@bayoo.net>
2022-08-09 06:52:34 +0200

    fix output variables

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "api_loadbalancer_ip" {
-  value       = k3s.api_load_balancer_ip
+  value       = module.k3s.api_load_balancer_ip
   description = "the IP of the API loadbalancer"
 }
 
 output "k3s_token" {
-  value       = k3s-k3s_token
+  value       = module.k3s.k3s_token
   description = "the secret k3s token"
 }
